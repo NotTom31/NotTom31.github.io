dca7f6b80c76c9041d385e5fe609cd0a3db8c0c1
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2023-12-06 02:33:16 -0800

    back to top arrow works but breaks the hover animation

--- scripts.js
+++ scripts.js
@@ -23,19 +23,41 @@ function scrollDown() {
 }
 
 function checkWindowSize() {
+    // Check window width and set nav-links display to block if greater than 700
     if (window.innerWidth > 700) {
         navLinks.style.display = "block";
     }
 }
 
 function handleScroll() {
-    const aboutMeSection = document.querySelector('.aboutme');
+const aboutMeSection = document.querySelector('.aboutme');
+const resumeSection = document.querySelector('.resume-section');
+const projectsSection = document.querySelector('.projects-header');
+const designSection = document.querySelector('.design-text-box');
+const homeTab = document.querySelector('.home-tab');
 
-    if (isElementInViewport(aboutMeSection)) {
-        aboutMeSection.classList.add('visible');
+const sections = [
+{ section: document.querySelector('.header'), tab: document.querySelector('.home-tab') },
+{ section: aboutMeSection, tab: document.querySelector('.about-tab') },
+{ section: resumeSection, tab: document.querySelector('.resume-tab') },
+{ section: projectsSection, tab: document.querySelector('.projects-tab') },
+{ section: designSection, tab: document.querySelector('.design-tab') },
+];
+
+sections.forEach(({ section, tab }) => {
+    if (isElementPartiallyInViewport(section, 50)) {
+        tab.classList.add('active');
     } else {
-        aboutMeSection.classList.remove('visible');
+        tab.classList.remove('active');
     }
+});
+
+// Toggle the opacity for the aboutme section
+if (isElementPartiallyInViewport(aboutMeSection, 50)) {
+    aboutMeSection.classList.add('visible');
+} else {
+    aboutMeSection.classList.remove('visible');
+}
 }
 
 document.addEventListener('scroll', handleScroll);
@@ -44,19 +66,74 @@ window.addEventListener('resize', checkWindowSize);
 handleScroll();
 checkWindowSize();
 
-function isElementInViewport(el) {
+function isElementPartiallyInViewport(el, percentVisible) {
     const rect = el.getBoundingClientRect();
+    const height = rect.height || el.offsetHeight;
+    const threshold = (height * percentVisible) / 100;
+
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top <= window.innerHeight - threshold &&
+        rect.bottom >= threshold
     );
 }
 
-function scrollDown() {
-    window.scrollBy({
-        top: window.innerHeight,
-        behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function () {
+    // Smooth scroll for navigation links
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+
+            if (targetId === 'home') {
+                // Scroll to the first section
+                window.scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+            } else {
+                // Scroll to the target section
+                const targetSection = document.getElementById(targetId);
+                window.scrollTo({
+                    top: targetSection.offsetTop - (window.innerHeight - targetSection.clientHeight) / 2,
+                    behavior: 'smooth'
+                });
+            }
+        });
     });
-}
+
+    function setResumeHeight() {
+        const resumeSection = document.querySelector('.resume-section');
+        const embedElement = resumeSection.querySelector('embed');
+
+        // Set a minimum height for the resume section
+        const minHeight = 400;
+
+        // Calculate the height based on the screen height
+        const newHeight = Math.max(window.innerHeight * 0.8, minHeight);
+
+        // Set the new height to the embed element
+        embedElement.style.height = newHeight + 'px';
+    }
+
+    // Call the function initially and on window resize
+    setResumeHeight();
+    window.addEventListener('resize', setResumeHeight);
+
+    // Adjust itch.io iframe width and height based on screen size
+    const itchIframes = document.querySelectorAll('.project-container iframe');
+
+    itchIframes.forEach(iframe => {
+        window.addEventListener('resize', function () {
+            if (window.innerWidth <= 700) {
+                iframe.width = "60%";
+                iframe.height = "200";
+            } else {
+                iframe.width = "554";
+                iframe.height = "169";
+            }
+        });
+    });
+});
